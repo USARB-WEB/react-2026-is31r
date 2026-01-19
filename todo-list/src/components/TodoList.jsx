197b1438e528f483e034994f58b521e6548db0ab
sergiuchilat <chilatsergiu@gmail.com>
2026-01-19 10:58:49 +0200

    useEffect

--- todo-list/src/components/TodoList.jsx
+++ todo-list/src/components/TodoList.jsx
@@ -1,33 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 
-function saveToLocalStorage(todos) {
+function saveToLocalStorage(todos, lastTodoId) {
     localStorage.setItem("myTodos", JSON.stringify(todos));
+    localStorage.setItem("lastTodoId", JSON.stringify(lastTodoId));
 }
 
 function loadFromLocalStorage() {
     if (typeof window === "undefined") {
         return [];
     }
-    
+
 
     console.log("Loading todos from localStorage");
 
     const todosString = localStorage.getItem("myTodos") || null;
+
     if (todosString) {
-        return JSON.parse(todosString);
+        try {
+            return {
+                lastTodoId: JSON.parse(localStorage.getItem("lastTodoId")) || 0,
+                todos: JSON.parse(todosString),
+            };
+        } catch (e) {
+            return {
+                lastTodoId: 0,
+                todos: [],
+            };
+        }
+
     }
-    return [];
+
+    return {
+        lastTodoId: 0,
+        todos: [],
+    };
 }
 
 export default function TodoList() {
+
+    const [loading, setLoading] = useState(true);
     const [newTodoText, setNewTodoText] = useState("");
-    const [myTodos, setMyTodos] = useState(loadFromLocalStorage());
+    const [myTodos, setMyTodos] = useState([]);
+    const [addBtnDisabled, setAddBtnDisabled] = useState(true);
+    const [fetching, setFetching] = useState(false);
+    const [backendResponseReceived, setBackendResponseReceived] = useState(false);
 
-    const [lastTodoId, setLastTodoId] = useState(3);
-
+    const [lastTodoId, setLastTodoId] = useState(0);
     function handleNewTodoBtnClicked() {
+        setFetching(true);
         const newTodo = {
             id: lastTodoId + 1,
             task: newTodoText,
@@ -37,11 +59,13 @@ export default function TodoList() {
         setNewTodoText("");
         setLastTodoId(lastTodoId + 1);
         console.log("New todo added:", myTodos);
-        saveToLocalStorage([...myTodos, newTodo]);
+        saveToLocalStorage([...myTodos, newTodo], lastTodoId + 1);
+        setTimeout(() => {
+            setFetching(false);
+        }, 1000);
     }
 
     function handleTodoDoneClicked(todoId) {
-        console.log("Todo done clicked:", todoId);
         const updatedTodos = myTodos.map((todo) => {
             if (todo.id === todoId) {
                 return { ...todo, isDone: true };
@@ -49,7 +73,7 @@ export default function TodoList() {
             return todo;
         });
         setMyTodos(updatedTodos);
-        saveToLocalStorage(updatedTodos);
+        saveToLocalStorage(updatedTodos, lastTodoId);
     }
 
     function handleBtnDeleteClicked(todoId) {
@@ -62,38 +86,81 @@ export default function TodoList() {
         setNewTodoText(e.target.value);
     }
 
+    useEffect(() => {
+        const { todos: initialTodos, lastTodoId: initialLastTodoId } = loadFromLocalStorage();
+        setMyTodos(initialTodos);
+        setLastTodoId(initialLastTodoId);
+        setTimeout(() => {
+            setBackendResponseReceived(true);
+        }, 2000);
+    }, []);
+
+    useEffect(() => {
+        console.log("New todo text changed:", newTodoText);
+        if (newTodoText.trim().length >= 3) {
+            setAddBtnDisabled(false);
+        } else {
+            setAddBtnDisabled(true);
+        }
+    }, [newTodoText]);
+
+    useEffect(() => {
+        if (backendResponseReceived) {
+            setLoading(false);
+        }
+    }, [backendResponseReceived]);
+
     return (
         <>
-            <ul>
-                {myTodos.map((todo) => (
-                    <li key={todo.id}>
-                        {todo.isDone ? "✔️" : ""}
-                        {!todo.isDone && (
-                            <>
-                                <button 
-                                    className="btn-done"
-                                    onClick={() => handleTodoDoneClicked(todo.id)}
-                                    >Done</button>
-                                <button
-                                    className="btn-remove"
-                                    onClick={() => handleBtnDeleteClicked(todo.id)}
-                                >Remove</button>
-                            </>
-                        )}
+            {loading && (
+                <div>Loading...</div>
+            )}
 
-                        <span>{todo.task}</span>
-                    </li>
-                ))}
-            </ul>
-            <input
-                type="text"
-                value={newTodoText}
-                onChange={handleNewTodoInputChange}
-            />
-            <button
-                className="btn-new"
-                onClick={handleNewTodoBtnClicked}
-            >New Todo</button>
+            {!loading && (
+                <>
+                    {myTodos && myTodos.length > 0 ? (
+                        <>
+                            <span>Total {myTodos?.length || 0} todos</span>
+                            <span> | </span>
+                            <span>
+                                Completed {myTodos.filter((todo) => todo.isDone).length} todos
+                            </span>
+                            <ul>
+                                {myTodos.map((todo) => (
+                                    <li key={todo.id}>
+                                        {todo.isDone ? "✔️" : ""}
+                                        {!todo.isDone && (
+                                            <>
+                                                <button
+                                                    className="btn-done"
+                                                    onClick={() => handleTodoDoneClicked(todo.id)}
+                                                >Done</button>
+                                                <button
+                                                    className="btn-remove"
+                                                    onClick={() => handleBtnDeleteClicked(todo.id)}
+                                                >Remove</button>
+                                            </>
+                                        )}
+
+                                        <span>{todo.task}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                    ) : 'No todos yet. Add your first todo!'}
+                    <input
+                        type="text"
+                        value={newTodoText}
+                        onChange={handleNewTodoInputChange}
+                    />
+                    {fetching && <div>Saving...</div>}
+                    <button
+                        className="btn-new"
+                        onClick={handleNewTodoBtnClicked}
+                        disabled={addBtnDisabled}
+                    >New Todo</button>
+                </>
+            )}
         </>
     );
 }
