48eb4cdbcf4b62c9cefbc07b33d4331070196164
sergiuchilat <chilatsergiu@gmail.com>
2026-01-19 08:54:19 +0200

    Create React app with 2 pages

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>This is a React app homepage</h1>
+      <a href="page2">Page 2</a>
+      <div>Hello world</div>
+    </>
+  );
+}
---
+++ my-app/src/app/page2/page.jsx
@@ -0,0 +1,9 @@
+export default function Page2() {
+  return (
+    <>
+      <h1>This is Page 2</h1>
+      <a href="/">Go back to Home</a>
+      <div>Welcome to Page 2!</div>
+    </>
+  );
+}
